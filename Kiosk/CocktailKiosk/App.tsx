b28dab451f80188e9531e0a8309cc57a1125bcf0
Russell Schwartz <russell.w.schwartz@gmail.com>
2022-06-20 20:11:47 -0400

    landscape app bare template

---
+++ Kiosk/CocktailKiosk/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+
+export default function App() {
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
